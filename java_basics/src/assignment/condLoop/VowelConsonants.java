1a4fa601da4f1638d19c58ffdce723603cbfae56
Kanish <kanish08.tech@gmail.com>
2025-07-28 11:13:12 +0530

    new proj

--- java_basics/src/assignment/condLoop/VowelConsonants.java
+++ java_basics/src/assignment/condLoop/VowelConsonants.java
@@ -8,6 +8,9 @@ public class VowelConsonants {
 
         char a = sc.next().trim().charAt(0);
         String str= "aeiouAEIOU";
+        for(int k = 1; k<=5; ++k){
+            System.out.println(k);
+        }
 
         if (str.indexOf(a) == -1){
             System.out.println("consonant");
